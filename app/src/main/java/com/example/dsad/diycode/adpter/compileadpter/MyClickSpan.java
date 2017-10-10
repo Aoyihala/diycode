2c397c7de122904fc9ba4bf5248f0a162d0add72
Aoyihala <1214504624@qq.com>
2017-10-10 17:08:01 +0800

    废弃

---
+++ app/src/main/java/com/example/dsad/diycode/adpter/compileadpter/MyClickSpan.java
@@ -0,0 +1,19 @@
+package com.example.dsad.diycode.adpter.compileadpter;
+
+import android.text.style.ClickableSpan;
+import android.view.View;
+
+/**
+ * 文本超链接点击事件
+ * Created by dsad on 2017/9/27.
+ */
+
+class MyClickSpan extends ClickableSpan
+{
+
+    @Override
+    public void onClick(View view)
+    {
+
+    }
+}

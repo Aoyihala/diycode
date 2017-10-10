eab315db1d497bb008095521231365ea13044646
Aoyihala <1214504624@qq.com>
2017-10-10 17:10:18 +0800

    eventbus注册后的基类
    提供给具有网络访问功能的页面

---
+++ app/src/main/java/com/example/dsad/diycode/base/RequsetActivity.java
@@ -0,0 +1,45 @@
+package com.example.dsad.diycode.base;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import org.greenrobot.eventbus.EventBus;
+
+/**
+ * 请求数据用的activivty
+ * Created by dsad on 2017/9/21.
+ */
+
+public class RequsetActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+    public void startActivity(Class clazz,boolean flag)
+    {
+        if (flag)
+        {
+            startActivity(new Intent(this,clazz));
+            finish();
+            return;
+        }
+        startActivity(new Intent(this,clazz));
+    }
+
+    //注册和注销event事件
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+}
